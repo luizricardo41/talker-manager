7ab00fae1eeda43b9b321025a2e54905c0b5f229
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-22 08:26:37 -0300

    fix: Corrigida função que valida os dados da requisição;
    Efetuada a correção na validação do campo talk.rate;

--- middlewares/validateData.js
+++ middlewares/validateData.js
@@ -21,7 +21,7 @@ function validateAge(req, res, next) {
 function validateTalk(req, res, next) {
   const { talk } = req.body;
   
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || talk.rate === undefined) {
     return res.status(400)
       .json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
