c3a314386ca21db851d1054499ad25ce4b622906
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-23 11:13:54 -0300

    refactor: refatoração para uso da rota criada

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
-const getTalkers = require('./middlewares/getTalkers');
-const getTalkerById = require('./middlewares/getTalkerById');
 const validateLogin = require('./middlewares/login');
-const createTalker = require('./middlewares/createTalker');
-const {
-  validateName, validateAge, validateTalk, validateDateAndRate,
-} = require('./middlewares/validateData');
-const validateToken = require('./middlewares/validateToken');
-const modifyTalker = require('./middlewares/modifyTalker');
-const deleteTalker = require('./middlewares/deleteTalker');
-const searchTalker = require('./middlewares/searchTalker');
+const talkerRoutes = require('./routes/talkerRoutes');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -24,31 +17,9 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', getTalkers);
-
-app.get('/talker/search', validateToken, searchTalker);
-
-app.get('/talker/:id', getTalkerById);
+app.use('/talker', talkerRoutes);
 
 app.post('/login', validateLogin);
-
-app.post('/talker',
-    validateToken,
-    validateName,
-    validateAge,
-    validateTalk,
-    validateDateAndRate,
-    createTalker);
-
-app.put('/talker/:id',
-  validateToken,
-  validateName,
-  validateAge,
-  validateTalk,
-  validateDateAndRate,
-  modifyTalker);
-
-app.delete('/talker/:id', validateToken, deleteTalker);
 
 app.listen(PORT, () => {
   console.log('Online');
